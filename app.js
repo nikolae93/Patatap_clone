bbb5e92a7d94a6dfffb7565565657e7b4abc8bcd
Nikola <nikolaekmedzic@yahoo.com>
2019-11-26 13:05:29 +0100

    Add basic backend

---
+++ app.js
@@ -0,0 +1,21 @@
+// Basic config
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+//Routes
+app.get("/",(req,res)=>{
+ res.render("./index.html");
+});
+
+
+// Server
+app.listen(port,()=>{
+  console.log("Server is up and running :D");
+})
+
+
+
